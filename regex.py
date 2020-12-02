5ca9260534a92487e962807a89c8a83b580b3f70
jackie-max <62066807+jackie-max@users.noreply.github.com>
2020-12-02 19:17:11 +0300

    Update regex.py

--- regex.py
+++ regex.py
@@ -6,11 +6,11 @@ line = file_in.readline()
 name_type = {}
 name_var = {}
 while line:
-    result = re.match(r'(int |short |long )?([a-zA-Z][a-zA-Z0-9]{0, 15}):=(([a-zA-Z][a-zA-Z0-9]{0, 15})|[0-9]+) ([+|-|*|/]) (([a-zA-Z][a-zA-Z0-9]{0, 15})|[0-9]+)', line)
+    result = re.match(r'(int |short |long )?([a-zA-Z][a-zA-Z0-9]{0,15}):=(([a-zA-Z][a-zA-Z0-9]{0,15})|[0-9]+) ([+|-|*|/]) (([a-zA-Z][a-zA-Z0-9]{0,15})|[0-9]+)', line)
     if result:
         file_out.write('valid\n')
-        name_type = re.match(r'(int|short|long)?', line)
-        name_var = re.match(r'([a-zA-Z][a-zA-Z0-9]{0, 15})', line)
+        name_type = re.match(r'(int |short |long)?', line)
+        name_var = re.match(r'([a-zA-Z][a-zA-Z0-9]{0,15})', line)
     else:
         file_out.write('invalid')
     line = file_in.readline()
